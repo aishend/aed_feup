abd1e2549bef14bf1b4f7ed3a34adc91e61cc5fc
Leandro <leandroafonsopm@gmail.com>
2025-10-01 17:00:51 +0100

    exercise 3 done

---
+++ class1/spaceOddity.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+bool isOdd(int number) {
+    if (number % 2) {
+        return true;
+    }
+    return false; 
+}
+
+
+int lastOddNumber; 
+int count = 0;
+
+int removeOds(int a[], int arraySize) {
+
+
+
+    for (int i = 0; i < arraySize; i++) {
+        if (isOdd(a[i])) {
+            count ++; 
+            lastOddNumber= a[i];
+        }
+    }
+
+
+    return count;
+}
+using namespace std; 
+
+int main() {
+    int arraySize;
+    cin >> arraySize;
+
+    int arrayA[arraySize]; 
+    
+    for (int i = 0; i < arraySize; i++) {
+        int tmp;
+        cin >> tmp; 
+        arrayA[i] = tmp; 
+    }
+    removeOds(arrayA, arraySize);
+    cout << count << " " << lastOddNumber << endl;
+    return 0; 
+}
